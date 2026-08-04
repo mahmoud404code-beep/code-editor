6f36b1224d2952e2430e1a5bbe78eb3b163f4d3b
codeweb <mahmoud404code@gmail.com>
2026-08-04 05:16:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function run() {
+    let input = document.getElementById("text-input").value;
+    let frame = document.getElementById("run2");
+
+    frame.contentDocument.open();
+    frame.contentDocument.write(input);
+    frame.contentDocument.close();
+}
+
+document.getElementById("text-input").addEventListener("keyup", run);
+
+run();
+
+function full() {
+    let input = document.getElementById("input");
+    let frame = document.getElementById("run");
+
+    input.style.display = input.style.display === "none" ? "block" : "none";
+
+    frame.style.height = frame.style.height === "100vh" ? "50vh" : "100vh";
+}
