2d377b7a36b5ea2781f4d52b74637d078ec7be98
mahmoud404code-beep <mahmoud404code@gmail.com>
2026-08-08 23:12:09 +0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function run() {
     let input = document.getElementById("text-input").value;
     let frame = document.getElementById("run2");
-
+    
     frame.contentDocument.open();
     frame.contentDocument.write(input);
     frame.contentDocument.close();
@@ -14,8 +14,14 @@ run();
 function full() {
     let input = document.getElementById("input");
     let frame = document.getElementById("run");
-
+    let button = event.target;
+    
+    button.innerHTML =
+        button.innerHTML === "تكبير" ?
+        "تصغير" :
+        "تكبير";
+    
     input.style.display = input.style.display === "none" ? "block" : "none";
-
+    
     frame.style.height = frame.style.height === "100vh" ? "50vh" : "100vh";
 }
